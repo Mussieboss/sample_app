b8974f9aea6c37b2286ffc28b9e8265262d1314b
Musya <workmusya@gmail.com>
2018-03-27 02:12:15 +0000

    make a basic User model (including secure password)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,6 +10,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+  end
+  
+  test "signup" do
+    get signup_path
+    assert_template 'users/new'
+    assert_select "h1", "Users#new"
   end
   # test "the truth" do
   #   assert true
